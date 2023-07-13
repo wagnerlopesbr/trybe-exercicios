a1cd206dde6f439055def5b8d89cba474ba8737a
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-13 20:26:20 -0300

    test1

--- fundamentos/7-higherOrderFunctions-JavaScriptES6/7.4-JavaScriptES6-spreadOperator-restParameters-objectDestructuring/monitoria/3-exercicio.js
+++ fundamentos/7-higherOrderFunctions-JavaScriptES6/7.4-JavaScriptES6-spreadOperator-restParameters-objectDestructuring/monitoria/3-exercicio.js
@@ -1,6 +1,5 @@
 const pessoas = require('./dados.js');
 // Encontre as pessoas que gostam de programar em JAVA
 const lookForJAVALovers = pessoas
-    .filter((pessoa) => pessoa
-        .filter((linguagem) => linguagem.includes('JAVA')));
+    .filter((pessoa) => pessoa.gostaDeProgramarEm.includes('JAVA'));
 console.log(lookForJAVALovers);
