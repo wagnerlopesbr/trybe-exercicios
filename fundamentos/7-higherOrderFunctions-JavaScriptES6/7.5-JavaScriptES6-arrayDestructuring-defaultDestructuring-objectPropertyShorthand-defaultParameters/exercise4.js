ed75c5208bd347850cc53136d61cfb1237b5e652
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-07-14 14:57:48 -0300

    WL fundamentos7.5 pós aula

---
+++ fundamentos/7-higherOrderFunctions-JavaScriptES6/7.5-JavaScriptES6-arrayDestructuring-defaultDestructuring-objectPropertyShorthand-defaultParameters/exercise4.js
@@ -0,0 +1,22 @@
+// Exercício 4
+// Escreva uma função getLastName que receba como parâmetro um objeto contendo informações de uma pessoa. Essa função deve retornar a propriedade lastName para o objeto passado, porém, caso o objeto não tenha essa propriedade, a função deve retornar a mensagem lastName não preenchido.
+
+const student1 = {
+    name: `Claudia`,
+    lastName: `Farias`,
+    age: 23,
+  }
+  
+  const student2 = {
+    name: `Vitor`,
+    age: 20,
+  }
+  
+  // escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+  const getLastName = (student) => {
+    const {lastName = 'lastName não preenchido'} = student;
+    return lastName;
+  }
+
+  console.log(getLastName(student1));
+  console.log(getLastName(student2));
